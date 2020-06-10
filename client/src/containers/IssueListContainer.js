ad14c62a0162f57469f23f13063b18fa1f570b27
baekCode <sosaer88@gmail.com>
2020-06-10 23:15:40 +0900

    [#15] feat: IssueListContainer 비지니스 로직 구현
    데이터 통신 및 데이터 전달

--- client/src/containers/IssueListContainer.js
+++ client/src/containers/IssueListContainer.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getIssues } from '../modules/issues';
 import Search from '../components/IssueList/Search';
 import List from '../components/IssueList/List';
 import Filters from '../components/IssueList/Filters';
 
 const IssueListContainer = () => {
+  const { loading, data, error } = useSelector(state => state.issues.issues);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getIssues());
+  }, [dispatch]);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러!!</div>;
+  if (!data) return null;
+
+  const list = data.reverse();
   return (
     <>
       <Search />
       <Filters />
-      <List />
+      <List list={list} />
     </>
   );
 };
