223f21561d616dbe164d9822f0722640faefefb3
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-10 22:05:47 +0900

    [#16] redux-thunk 및 Provider 적용
    Title >
    * redux-thunk 및 Provider 적용
    
    Description >
    * 비동기 API 를 위해 redux-thunk 를 적용
    * 컨테이너에서 useSelector 를 통해 상태값을 얻어올 수 있도록 Provider
    적용

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './modules';
+import ReduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const customHistory = createBrowserHistory();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    ReduxThunk.withExtraArgument({ history: customHistory }),
+    logger
+  )
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router history={customHistory}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root'),
 );
 serviceWorker.unregister();
