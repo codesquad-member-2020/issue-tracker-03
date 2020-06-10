af618eae610bebd3e997ac09a5c72a838f66e0bb
baekCode <sosaer88@gmail.com>
2020-06-10 23:13:48 +0900

    [#15] feat: 리덕스 스토어 구현 & 리덕스 미들웨어 구현
    - createStore(redux) : 스토어 생성
    - applyMiddleware(redux) : 미들웨어 결합

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './modules';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger)),
+);
+
+window.store = store;
+console.log(store.getState());
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 serviceWorker.unregister();
