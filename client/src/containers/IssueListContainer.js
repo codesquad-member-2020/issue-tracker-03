783225e643b22e8727f269e1a6edc8ce51888ebf
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-10 22:08:06 +0900

    [FE] feat: 샘플 IssueListContainer 작성
    Title >
    * 샘플 IssueListContainer 작성
    
    Description >
    * 이슈목록 관련 데이터를 요청하게 될 IssueListContainer 에서
      api/issueList 의 getIssueList 를 요청하도록 작성

--- client/src/containers/IssueListContainer.js
+++ client/src/containers/IssueListContainer.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getIssueList } from '../modules/issueList';
 import Search from '../components/IssueList/Search';
 import List from '../components/IssueList/List';
 import Filters from '../components/IssueList/Filters';
 
 const IssueListContainer = () => {
+  const { data, loading, error } = useSelector(state => state.issueList.issueList);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getIssueList());
+  }, [dispatch]);
+
+  if (loading && !data) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
+
   return (
     <>
       <Search />
