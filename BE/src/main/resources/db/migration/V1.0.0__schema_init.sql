159049f2d3bb2f50d0fc2089567ca6a97378a8e7
Han <quaint3@gmail.com>
2020-06-10 16:10:39 +0900

    [#9] chore : schema, mock data init

--- BE/src/main/resources/db/migration/V1.0.0__schema_init.sql
+++ BE/src/main/resources/db/migration/V1.0.0__schema_init.sql
@@ -1,7 +1,24 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS issue;
 
 CREATE TABLE user
 (
-    id INT AUTO_INCREMENT,
+    id       BIGINT AUTO_INCREMENT,
+    email    VARCHAR(50),
+    user_id  VARCHAR(50),
+    nickname VARCHAR(50),
+    password VARCHAR(100),
     PRIMARY KEY (id)
+);
+
+CREATE TABLE issue
+(
+    id              BIGINT AUTO_INCREMENT,
+    title           VARCHAR(50),
+    contents        VARCHAR(500),
+    is_open         BOOLEAN   DEFAULT TRUE,
+    created_time_at TIMESTAMP DEFAULT NOW(),
+    user_id         BIGINT,
+    PRIMARY KEY (id),
+    CONSTRAINT issue_has_user_id FOREIGN KEY (user_id) REFERENCES user (id)
 )
---
+++ BE/src/main/resources/db/migration/V1.0.1__add_mock_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user(email, user_id, password, nickname)
+VALUES ('han@gmail.com', 'han1234', '1234', 'han'),
+       ('zello@gmail.com', 'zello1234', '1234', 'zello'),
+       ('sunny@gmail.com', 'sunny1234', '1234', 'sunny'),
+       ('baekco@gmail.com', 'baekco1234', '1234', 'baekco');
+
+INSERT INTO issue(title, contents, user_id)
+VALUES ('첫번째 이슈입니다', '이슈1', 1),
+       ('두번째 이슈입니다', '이슈2', 2),
+       ('세번째 이슈입니다', '이슈3', 3),
+       ('네번째 이슈입니다', '이슈4', 4);
