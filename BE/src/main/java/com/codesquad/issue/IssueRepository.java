c19fe2d327f5275fb3749993b82a6a5ac19ce3d2
Han <quaint3@gmail.com>
2020-06-11 20:02:29 +0900

    [#9] feat: 열린, 닫힌 이슈를 위한 repository query 설정

--- BE/src/main/java/com/codesquad/issue/IssueRepository.java
+++ BE/src/main/java/com/codesquad/issue/IssueRepository.java
@@ -1,7 +1,12 @@
 package com.codesquad.issue;
 
 import com.codesquad.issue.domain.Issue;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+  List<Issue> findAllByIsOpenTrue();
+
+  List<Issue> findAllByIsOpenFalse();
 }
