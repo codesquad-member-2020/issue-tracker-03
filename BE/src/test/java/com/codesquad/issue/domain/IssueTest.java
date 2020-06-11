ae470892b934b863c730ce22b23d4e9326f3c3bc
Han <quaint3@gmail.com>
2020-06-11 17:41:53 +0900

    [#9] test: 생성, 저장 이슈 테스트
    생성한 이슈와, DB에 저장된 이슈가 물리적으로 같음을 증명하는 테스트

--- BE/src/test/java/com/codesquad/issue/domain/IssueTest.java
+++ BE/src/test/java/com/codesquad/issue/domain/IssueTest.java
@@ -33,12 +33,24 @@ class IssueTest {
   }
 
   @Test
-  @DisplayName("Title 없이 이슈 생성")
+  @DisplayName("Title 없이 이슈 생성시 에러 발생")
   void create_issue_without_title() {
     Issue issue = Issue.builder()
         .contents("안녕하세요")
         .build();
 
     assertThrows(Exception.class, () -> issueRepository.save(issue));
+  }
+
+  @Test
+  @DisplayName("생성한 이슈와, 저장한 이슈 비교")
+  void create_save() {
+    Issue issue = Issue.builder()
+        .title("테스트입니다")
+        .contents("안녕하세요")
+        .build();
+
+    Issue saved = issueRepository.save(issue);
+    assertThat(issue).isEqualTo(saved);
   }
 }
