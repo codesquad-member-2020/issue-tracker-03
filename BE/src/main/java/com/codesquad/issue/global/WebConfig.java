387b1be97cfd33ffaabd2bfb03a8380a1102e296
Han <quaint3@gmail.com>
2020-06-11 17:26:05 +0900

    [#9] feat: web config 설정
    cors 설정
    날짜 포멧팅 설정

---
+++ BE/src/main/java/com/codesquad/issue/global/WebConfig.java
@@ -0,0 +1,40 @@
+package com.codesquad.issue.global;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private static final String DATE_FORMAT = "yyyy-MM-dd";
+  private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+  private static final String TIME_ZONE = "Asia/Seoul";
+
+  @Bean
+  public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+    return builder -> {
+      builder.simpleDateFormat(DATE_TIME_FORMAT);
+      builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+      builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+      builder.serializers(new ZonedDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+      builder.timeZone(TimeZone.getTimeZone(TIME_ZONE));
+    };
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTION")
+        .allowedOrigins("*")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
+}
