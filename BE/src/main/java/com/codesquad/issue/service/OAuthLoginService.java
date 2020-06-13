1e68981cea30e773209fa44f06c612c63c99b2e8
Sosah <thstkd92@gmail.com>
2020-06-13 15:52:36 +0900

    [#9] fix: CORS에러를 막기 위한 WebConfig.class 추가
    - 그 이전 WebConfig는 Github에 해당하는 내용이 있어 GithubConfig로 수정

--- BE/src/main/java/com/codesquad/issue/service/OAuthLoginService.java
+++ BE/src/main/java/com/codesquad/issue/service/OAuthLoginService.java
@@ -32,6 +32,7 @@ public class OAuthLoginService {
     private final String GITHUB_API = "https://api.github.com/user";
 
     private final GithubOAuth githubOAuth;
+    private final RestTemplate restTemplate;
 
     @Value("${host}")
     private String mainUrl;
@@ -62,20 +63,19 @@ public class OAuthLoginService {
         requestHeaders.setAll(requestHeader);
 
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestHeaders, requestAccess());
-        ResponseEntity<GithubAccessToken> response = new RestTemplate().
+        ResponseEntity<GithubAccessToken> response = restTemplate.
                 postForEntity(githubOAuth.getUri(), request, GithubAccessToken.class);
         return response.getBody();
     }
 
     public AccountApiRequest getAccountByToken(String accessToken, HttpHeaders headers) {
         headers.set("Authorization", accessToken);
-        AccountApiRequest userApiRequest = Optional.ofNullable(new RestTemplate().exchange(GITHUB_API, HttpMethod.GET,
+        AccountApiRequest userApiRequest = Optional.ofNullable(restTemplate.exchange(GITHUB_API, HttpMethod.GET,
                 new HttpEntity<>(headers), AccountApiRequest.class).getBody())
                 .orElseThrow(() -> new UserNotFoundException("요청한 github user를 찾을 수 없습니다."));
         if (userApiRequest.getEmail() == null) {
             userApiRequest.setEmail(getEmailByToken(headers));
         }
-
         return userApiRequest;
     }
 
@@ -88,7 +88,7 @@ public class OAuthLoginService {
     }
 
     private String getEmailByToken(HttpHeaders headers) {
-        ResponseEntity<String> email = new RestTemplate().exchange(GITHUB_API + "/emails", HttpMethod.GET,
+        ResponseEntity<String> email = restTemplate.exchange(GITHUB_API + "/emails", HttpMethod.GET,
                 new HttpEntity<>(headers), String.class);
 
         return jsonParser(email.getBody());
