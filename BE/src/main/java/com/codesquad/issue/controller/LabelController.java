4e532567ad6a048f9842913cd1c175fc65c541f5
Sosah <thstkd92@gmail.com>
2020-06-13 15:53:40 +0900

    [#9] Label 기본 로직 추가
    - Label Entity추가 추후에 로직 추가해야 합니다.

---
+++ BE/src/main/java/com/codesquad/issue/controller/LabelController.java
@@ -0,0 +1,14 @@
+package com.codesquad.issue.controller;
+
+import com.codesquad.issue.domain.label.LabelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class LabelController {
+
+    private final LabelRepository labelRepository;
+
+
+}
