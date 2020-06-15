e6fa14e0bd043ba851c6b434ebac2f2ba418ef91
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-15 14:47:19 +0900

    [#26] modify: Header 및 Footer 를 항상 포함하도록 수정
    Title >
    * Header 및 Footer 를 항상 포함하도록 수정
    
    Description >
    * 모든 페이지에서는 항상 Header 및 Footer 를 포함하므로
      각 Page 에서 Header 및 Footer 를 포함하는 구조가 아닌
      AppRouter 에서 포함하여 모든 페이지에서 Header 및 Footer 를
      포함하는 코드를 작성하지 않아도 되도록 수정

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -12,9 +12,30 @@ import MilestoneCreatePage from '@Pages/MilestoneCreatePage';
 import MilestoneListPage from '@Pages/MilestoneListPage';
 import MilestoneModifyPage from '@Pages/MilestoneModifyPage';
 import LoginPage from '@Pages/LoginPage';
+import Cookies from 'universal-cookie';
+
+import { setLoginInfo, resetLoginInfo } from '../modules/login';
+import { useDispatch } from 'react-redux';
+
+import HeaderContainer from '@Containers/HeaderContainer'
+import FooterContainer from '@Containers/FooterContainer'
 
 function AppRouter() {
+  const dispatch = useDispatch();
+
+  const cookies = new Cookies();
+  const loginInfo = cookies.get('jwt');
+  
+  if (loginInfo !== undefined) {
+    dispatch(setLoginInfo(loginInfo));
+  }
+  else {
+    dispatch(resetLoginInfo());
+  }
+
   return (
+    <>
+    <HeaderContainer />
     <Router>
       <Route exact={true} path="/" component={IssueListPage} />
       <Route path="/issue-detail/:issueId/" component={IssueDetailPage} />
@@ -25,6 +46,8 @@ function AppRouter() {
       <Route path="/milestone-modify/:milestoneId/" component={MilestoneModifyPage} />
       <Route path="/login/" component={LoginPage} />
     </Router>
+    <FooterContainer />
+    </>
   );
 }
 
