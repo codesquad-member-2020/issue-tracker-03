7e614730657ac095d29519d529d3a2d66cc5a560
baekCode <sosaer88@gmail.com>
2020-06-15 22:12:41 +0900

    Revert "[#042] chore: 디렉토리 수정"
    This reverts commit 83f71fb9328e987148bc26e86ef3e42891d13c01.

---
+++ client/src/lib/styles/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  path {
+    fill: currentColor;
+  }
+`;
