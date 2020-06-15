cfeec6c7697cd38f6d051c82f5f13da608a7ffdd
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-15 14:56:51 +0900

    [#26] modify: Header 및 Footer container 의 위치 이동
    Title >
    * Header 및 Footer container 의 위치 이동
    
    Description >
    * AppRouter 에서 렌더링 되던 Header 및 Footer Container 의 위치를
      App 으로 변경.
      (AppRouter 에서 Header, Footer 를 렌더링하는것은 적절하지
      않다고 판단)

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -12,30 +12,9 @@ import MilestoneCreatePage from '@Pages/MilestoneCreatePage';
 import MilestoneListPage from '@Pages/MilestoneListPage';
 import MilestoneModifyPage from '@Pages/MilestoneModifyPage';
 import LoginPage from '@Pages/LoginPage';
-import Cookies from 'universal-cookie';
-
-import { setLoginInfo, resetLoginInfo } from '../modules/login';
-import { useDispatch } from 'react-redux';
-
-import HeaderContainer from '@Containers/HeaderContainer'
-import FooterContainer from '@Containers/FooterContainer'
 
 function AppRouter() {
-  const dispatch = useDispatch();
-
-  const cookies = new Cookies();
-  const loginInfo = cookies.get('jwt');
-  
-  if (loginInfo !== undefined) {
-    dispatch(setLoginInfo(loginInfo));
-  }
-  else {
-    dispatch(resetLoginInfo());
-  }
-
   return (
-    <>
-    <HeaderContainer />
     <Router>
       <Route exact={true} path="/" component={IssueListPage} />
       <Route path="/issue-detail/:issueId/" component={IssueDetailPage} />
@@ -46,8 +25,6 @@ function AppRouter() {
       <Route path="/milestone-modify/:milestoneId/" component={MilestoneModifyPage} />
       <Route path="/login/" component={LoginPage} />
     </Router>
-    <FooterContainer />
-    </>
   );
 }
 
