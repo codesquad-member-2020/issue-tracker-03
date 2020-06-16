aad9dd3b084b707f1bb3b0f0a03ea917632a70e0
baekCode <sosaer88@gmail.com>
2020-06-16 21:56:34 +0900

    [#57] feat: 이슈 id 네트워크 통신

--- client/src/api/issueList.js
+++ client/src/api/issueList.js
@@ -4,3 +4,10 @@ export const getIssueList = async filterUrl => {
   const response = await axios.get(process.env.REACT_APP_API_ISSUELIST);
   return response.data;
 };
+
+export const getIssueById = async id => {
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_ISSUELIST}/${id}`,
+  );
+  return response.data;
+};
