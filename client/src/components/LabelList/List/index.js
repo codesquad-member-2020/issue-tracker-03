268dbc327b9168d7cf43a8ba7d0254e1b6a1fca3
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-19 00:22:54 +0900

    [#58] feat: LabelList 컴포넌트 및 컨테이너 수정
    Title >
    * LabelList 컴포넌트 및 컨테이너 수정
    
    Description >
    * 로그인 여부에 따라 'Save changes' 등의 기능의 동작 유/무 를 설정
      하도록 구현
    * 사용자가 Normal 상태에서 Edit / Expanded 상태에서 Cancel 버튼을
      누름에 따라서 Normal <-> Expanded 상태로 바뀔 수 있도록 구현

--- client/src/components/LabelList/List/index.js
+++ client/src/components/LabelList/List/index.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
 
-import Article from '@Components/LabelList/List/Article';
+import ArticleNormal from "@Components/LabelList/List/ArticleNormal";
+import ArticleExpanded from "@Components/LabelList/List/ArticleExpanded";
 
-const List = ({ labelList }) => {
+const List = ({
+  labelList,
+  expandedList,
+  onEditButtonClick,
+  onDeleteButtonClick,
+  onCancelButtonClick,
+  onSaveButtonClick,
+  changeEnabled,
+  submitText
+}) => {
+  console.log(expandedList.get(0));
   return (
     <div>
-      {labelList.map(article => (
-        <Article
-          name={article.name}
-          color={article.color}
-          description={article.description}
-        />
-      ))}
+      {labelList.map((article, index) => {
+        if (!expandedList.get(index))
+          return (
+            <ArticleNormal
+              key={index}
+              index={index}
+              labelName={article.name}
+              labelColor={article.color}
+              labelDescription={article.description}
+              onEditButtonClick={onEditButtonClick}
+              onDeleteButtonClick={onDeleteButtonClick}
+            />
+          );
+        else
+          return (
+            <ArticleExpanded
+              key={index}
+              index={index}
+              labelName={article.name}
+              labelNamePlaceHolder="Label name"
+              labelColor={article.color}
+              labelDescription={article.description}
+              labelDescriptionPlaceHolder="Description (optional)"
+              onCancelButtonClick={onCancelButtonClick}
+              onSaveButtonClick={onSaveButtonClick}
+              changeEnabled={changeEnabled}
+              submitText={submitText}
+            />
+          );
+      })}
     </div>
   );
 };
--- client/src/containers/LabelListContainer.js
+++ client/src/containers/LabelListContainer.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import {
   getLabelList,
@@ -6,16 +7,65 @@ import {
 import styled from 'styled-components';
 import List from '@Components/LabelList/List/index'
 import Counter from '@Components/LabelList/Counter/index'
+import ButtonPanel from '@Components/Common/ButtonPanel'
+import ArticleExpanded from "@Components/LabelList/List/ArticleExpanded";
+
+const ButtonPanelWrap = styled.div`
+  padding: 20px 0;
+`;
 
 const IssueCreatecontainer = () => {
+  const history = useHistory();
+  const [expandedList, setExpandedList] = useState(new Map());
+  const [newLabelTabVisible, setNewLabelTabVisible] = useState(false);
+  const { loginStateInfo } = useSelector(({ login }) => login);
   const { data, loading, error } = useSelector(
     state => state.labelList.labelList,
   );
   const dispatch = useDispatch();
 
   useEffect(() => {
+    if (data) {
+      const labelList = data.response;
+      labelList.expanded = new Map();
+      labelList.forEach((element, index) => {
+        expandedList.set(index, false);
+      });
+      return
+    };
     dispatch(getLabelList());
-  }, [dispatch]);
+  }, [dispatch, data]);
+
+  const onNewLabelButtonClickHandler = () => {
+    setNewLabelTabVisible(true);
+  }
+
+  const onMilestoneButtonClickHandler = () => {
+    history.push('/milestone-list');
+  }
+
+  const onEditButtonClickHandler = (index) => {
+    setExpandedList(new Map(expandedList.set(index, true)))
+  }
+
+  const onDeleteButtonClickHandler = (index) => {
+    console.log("onDeleteButtonClickHandler", index);
+  }
+
+  const onCancelButtonClickHandler = (index) => {
+    setExpandedList(new Map(expandedList.set(index, false)))
+  }
+
+  const onSaveButtonClickHandler = (index, contents) => {
+    console.log("onSaveButtonClickHandler", index, contents);
+  }
+
+  const onCancelCreateNewLabelHandler = () => {
+    setNewLabelTabVisible(false);
+  }
+
+  const onCreateNewLabelHandler = () => {
+  }
 
   if (loading && !data) return <div>로딩중...</div>;
   if (error) return <div>에러 발생!</div>;
@@ -25,8 +75,33 @@ const IssueCreatecontainer = () => {
 
   return (
     <>
-      <Counter count={labelList.length}/>
-      <List labelList={labelList}></List>
+      <ButtonPanelWrap>
+        <ButtonPanel
+          onSubmitButtonClick={onNewLabelButtonClickHandler}
+          submitButtonText="New label"
+          submitButtonEnabled={true}
+          onMilestoneButtonClick={onMilestoneButtonClickHandler}
+          labelButtonSelected={true}
+        />
+      </ButtonPanelWrap>
+      {newLabelTabVisible && (
+        <ArticleExpanded
+          onCancelButtonClick={onCancelCreateNewLabelHandler}
+          onSaveButtonClick={onCreateNewLabelHandler}
+          submitText="Create label"
+        />
+      )}
+      <Counter count={labelList.length} />
+      <List
+        labelList={labelList}
+        expandedList={expandedList}
+        onEditButtonClick={onEditButtonClickHandler}
+        onDeleteButtonClick={onDeleteButtonClickHandler}
+        onSaveButtonClick={onSaveButtonClickHandler}
+        onCancelButtonClick={onCancelButtonClickHandler}
+        changeEnabled={loginStateInfo}
+        submitText="Save changes"
+      ></List>
     </>
   );
 };
