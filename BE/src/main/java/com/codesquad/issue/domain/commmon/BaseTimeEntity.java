ea422db6e2c8b78c4df313d90feb364da9dc9a68
Han <quaint3@gmail.com>
2020-06-18 17:03:44 +0900

    [#34] feat: BaseTimeEntity Getter 추가

--- BE/src/main/java/com/codesquad/issue/domain/commmon/BaseTimeEntity.java
+++ BE/src/main/java/com/codesquad/issue/domain/commmon/BaseTimeEntity.java
@@ -3,12 +3,14 @@ package com.codesquad.issue.domain.commmon;
 import java.time.LocalDateTime;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseTimeEntity {
 
     @CreatedDate
