b15aecd847d0bcfdd25f825cf66beddcb29a774e
Sosah <thstkd92@gmail.com>
2020-06-19 14:52:45 +0900

    [#8] fix: codedeploy 권한 수정 및 배포스크립트 경로 수정

--- BE/deploy.sh
+++ BE/deploy.sh
@@ -13,10 +13,10 @@ if [ -z $CURRENT_PID ]
 then
   echo "> 종료할것 없음."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH 2>&1 &
