944ed66ac5080ff754f342b6449a9164e046b2f7
baekCode <sosaer88@gmail.com>
2020-06-22 22:07:30 +0900

    [#95] feat: 사이드패널 영역 모듈 구현

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import issueList from './issueList';
 import login from './login';
 import labelList from './labelList';
+import sidePanels from './sidePanels';
 
-const rootReducer = combineReducers({ issueList, login, labelList });
+const rootReducer = combineReducers({ issueList, login, labelList, sidePanels });
 
 export default rootReducer;
