ef4f47db3825cf73b5db485602f40d58891dffca
Han <quaint3@gmail.com>
2020-06-22 22:26:28 +0900

    [#82]: feat: image 컨트롤러 생성

---
+++ BE/src/main/java/com/codesquad/issue/controller/ImageController.java
@@ -0,0 +1,27 @@
+package com.codesquad.issue.controller;
+
+import com.codesquad.issue.global.utils.FileUtils;
+import com.codesquad.issue.service.S3Service;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("images")
+public class ImageController {
+
+    private final S3Service s3Service;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String upload(@RequestPart(value = "file") MultipartFile file) throws IOException {
+        return s3Service.upload(FileUtils.toFile(file), "issue-003");
+    }
+}
