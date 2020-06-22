43da9c832c40cbe595a1e43136678c82486884a5
baekCode <sosaer88@gmail.com>
2020-06-22 22:08:25 +0900

    [#95] feat: 사이드패널 컴포넌트 구현

---
+++ client/src/components/SidePanels/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Panel from './Panel';
+import { getAssignees, getLabels, getMilestones } from '../../modules/sidePanels';
+
+const SidePanels = ({ data }) => {
+  console.log(data);
+  const onClickHandler = (value, type) => {
+    console.log('[click] ', value, type);
+  };
+  return (
+    <div>
+      <Panel type={'labels'} value={'name'} dispatchCallback={getLabels} onClickHandler={onClickHandler} />
+      <Panel type={'milestones'} value={'title'} dispatchCallback={getMilestones} onClickHandler={onClickHandler} />
+    </div>
+  );
+};
+
+export default SidePanels;
