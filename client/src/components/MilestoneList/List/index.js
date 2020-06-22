1246c75e9d830e47835e49baf5b7efdb8724207a
Angelo-Valrentin <valrentin.angelo@gmail.com>
2020-06-22 11:50:29 +0900

    [#105] feat: MilestoneList 관련 컴포넌트 작성
    Tile >
    * MilestoneList 관련 컴포넌트 작성
    
    Description >
    * List/Article: 단위 마일스톤에 대한 정보를 나타내는 컴포넌트
    * List/index: Article 들을 렌더링 하는 컴포넌트
    * Counter/index: open, close 카운트를 보여주는 컴포넌트

---
+++ client/src/components/MilestoneList/List/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Article from "@Components/MilestoneList/List/Article";
+
+const List = ({
+
+}) => {
+  return (
+    <>
+      <Article></Article>
+    </>
+  );
+};
+
+export default List;
