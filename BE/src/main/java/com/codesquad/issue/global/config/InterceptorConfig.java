ca9958af9ea1a4aa386c0346c0e72286fbea81bd
Han <quaint3@gmail.com>
2020-06-24 22:34:39 +0900

    [#132] feat: InterceptorConfig mapping url 추가

--- BE/src/main/java/com/codesquad/issue/global/config/InterceptorConfig.java
+++ BE/src/main/java/com/codesquad/issue/global/config/InterceptorConfig.java
@@ -15,7 +15,9 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(basicInterceptor)
-                .addPathPatterns("/issues/**");
-
+                .addPathPatterns("/issues/**")
+                .addPathPatterns("/labels/**")
+                .addPathPatterns("/comments/**")
+                .addPathPatterns("/milestones/**");
     }
 }
