71738e1ac27d787f14fc1418a20c0d911d67697f
Han <quaint3@gmail.com>
2020-06-24 21:14:38 +0900

    [#125]: chore: 시현을 위한 데이터 정리
    프로젝트를 위해 임시로 넣어두었던 mockdata 삭제
    깃헙에서 기본적으로 제공해주는 라벨 추가
    마일스톤 due date 시간 변경

--- BE/src/main/resources/db/migration/V1.0.1__add_mock_init.sql
+++ BE/src/main/resources/db/migration/V1.0.1__add_mock_init.sql
@@ -1,34 +1,15 @@
-INSERT INTO account(email, login, name, avatar_url)
-VALUES ('sunny@gmail.com', 'sunny1234', 'sunny', 'url');
-
-INSERT INTO issue(title, account_id)
-VALUES ('첫번째 이슈입니다', 1),
-       ('두번째 이슈입니다', 1),
-       ('세번째 이슈입니다', 1),
-       ('네번째 이슈입니다', 1);
-
-INSERT INTO issue(title, account_id, is_open)
-VALUES ('첫번째 닫힌 이슈입니다', 1, false),
-       ('두번째 닫힌 이슈입니다', 1, false),
-       ('세번째 닫힌 이슈입니다', 1, false);
-
 INSERT INTO label(name, description, color)
 VALUES ('bug', 'Something isn''t working', '#d73a4a'),
        ('documentation', 'Improvements or additions to documentation', '#0075ca'),
-       ('question', 'Further information is requested', '#d876e3'),
+       ('duplicate', 'This issue or pull request already exists', '#cfd3d7'),
+       ('enhancement', 'New feature or request', '#a2eeef'),
+       ('good first issue', 'Good for newcomers', '#7057ff'),
+       ('help wanted', 'Extra attention is needed', '#008672'),
        ('invalid', 'This doesn''t seem right', '#e4e669'),
-       ('help wanted', 'Extra attention is needed', '#008672');
-
-INSERT INTO comment(contents, issue_id, account_id)
-VALUES ('댓글1', 1, 1),
-       ('댓글2', 1, 1),
-       ('댓글3', 1, 1);
-
-INSERT INTO issue_label(issue_id, label_id)
-VALUES (1, 1),
-       (1, 2);
+       ('question', 'Further information is requested', '#d876e3'),
+       ('wontfix', 'This will not be worked on', '#ffffff');
 
 INSERT INTO milestone (name, description, due_date)
-VALUES ('1주차', 'scrum', '2020-06-27'),
-       ('2주차', 'coding', '2020-07-01'),
-       ('3주차', 'deploy', '2020-07-31');
+VALUES ('1주차', 'scrum', '2020-06-28'),
+       ('2주차', 'coding', '2020-07-05'),
+       ('3주차', 'deploy', '2020-07-12');
