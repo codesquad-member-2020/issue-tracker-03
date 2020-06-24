db322c35bde5c7be0b99d6802d4e7931ccc1f0d2
Han <quaint3@gmail.com>
2020-06-24 21:57:06 +0900

    [#132] feat: Interceptor클래스, config 설정

---
+++ BE/src/main/java/com/codesquad/issue/global/config/InterceptorConfig.java
@@ -0,0 +1,21 @@
+package com.codesquad.issue.global.config;
+
+import com.codesquad.issue.global.security.BasicInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final BasicInterceptor basicInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(basicInterceptor)
+                .addPathPatterns("/issues/**");
+
+    }
+}
--- BE/src/main/java/com/codesquad/issue/global/config/WebConfig.java
+++ BE/src/main/java/com/codesquad/issue/global/config/WebConfig.java
@@ -3,15 +3,14 @@ package com.codesquad.issue.global.config;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.time.format.DateTimeFormatter;
-import java.util.TimeZone;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
