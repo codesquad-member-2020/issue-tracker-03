2a16d7cdf049cad0c785dd40388edcf4fdb918ed
Sosah <thstkd92@gmail.com>
2020-06-25 15:21:55 +0900

    [#141] fix: milestone에 description 추가

--- BE/src/main/java/com/codesquad/issue/domain/milestone/response/MilestoneResponse.java
+++ BE/src/main/java/com/codesquad/issue/domain/milestone/response/MilestoneResponse.java
@@ -12,12 +12,15 @@ public class MilestoneResponse {
 
     private final String name;
 
+    private final String description;
+
     private final LocalDate dueDate;
 
     @Builder
-    private MilestoneResponse(Long id, String name, LocalDate dueDate) {
+    private MilestoneResponse(Long id, String name, LocalDate dueDate, String description) {
         this.id = id;
         this.name = name;
         this.dueDate = dueDate;
+        this.description = description;
     }
 }
